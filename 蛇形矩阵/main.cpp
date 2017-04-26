70e9fe743309999658515d0ded5d168f1a55d6ca
king <764666894@qq.com>
2017-04-26 22:23:01 +0800

    hello

--- 蛇形矩阵/main.cpp
+++ 蛇形矩阵/main.cpp
@@ -1,54 +1,8 @@
 #include <iostream>
-int main() {
-using namespace std;
-int draw_ji(int n,int arr[][100]);
-int draw_ou(int n,int arr[][100]);
-int k;
-cin>>k;
-int arr[100][100];
-	if(k%2)
-	draw_ji(k,arr);
-	else
-	draw_ou(k,arr);
-	for(int i=1;i<=k;i++)
-	{
-	for(int j=1;j<=k;j++)
-	cout<<arr[i][j]<<" ";
-	cout<<'\n';
-    }
-    cin.get();
+int main()
+{int x=0;int y=2;
+if(x=0)y++;
+	std::cout<<y;
+	std::cin.get();
 	return 0;
-}
- draw_ji(int n,int arr[][100])
-{   if(n==1) 	
-{
-arr[1][1]=1;
-return 0;
-}
-else{
-int draw_ou(int n,int arr[][100]);
-	draw_ou(n-1,arr);
-	arr[1][1]=(n-1)^2+1;
-	for(int j=2;j<=n;j++)
-	arr[j][1]=arr[1][j-1]+1;
-	for(int i=2;i<=n;i++)
-	arr[n][i]=arr[n][i-1]+1;
-}
-}
-int draw_ou(int n,int arr[][100])
-{   if(n==1) 	{
-arr[1][1]=1;
-return 0;
-}
-else
-{
-int draw_ji(int n,int arr[][100]);
-	draw_ji(n-1,arr);
-	arr[n][n]=(n-1)^2+1;
-	for(int j=n-1;j>=1;j--)
-	arr[j][n]=arr[j+1][n]+1;
-	for(int i=n-1;i>=1;i--)
-	arr[1][i]=arr[1][i-1]+1;
-}
  } 
-
