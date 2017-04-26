2d5758218fec45802fff5fa284d7591f17eec569
king <764666894@qq.com>
2017-04-26 19:48:14 +0800

    cartor表

---
+++ cantor/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+int main()
+{
+	using std::cout;
+	using std::cin;
+	int i=1;
+	int k1,k2,n;
+	cin>>n;
+	int sum=0;
+	while(sum<n)
+	{
+		sum+=i;
+		i++;
+	}
+	i=i-1;
+	k1=i-sum+n;
+	k2=1+sum-n;
+	if(i%2) 
+	cout<<k2<<'/'<<k1;
+	else
+	cout<<k1<<'/'<<k2;
+	return 0;
+ } 
