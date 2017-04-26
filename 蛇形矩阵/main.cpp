45af64729a73b2f2c913142b8a16ad3e41060985
king <764666894@qq.com>
2017-04-26 22:23:20 +0800

    Revert "成功运行但WA"
    This reverts commit 1f6890bc3c494c70dc416d1c55abec061686aa11.

--- 蛇形矩阵/main.cpp
+++ 蛇形矩阵/main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 int main() {
-using namespace std;
-int draw_ji(int n,int arr[][100]);
-int draw_ou(int n,int arr[][100]);
+	using namespace std;
+void draw_ji(int);
+void draw_ou(int);
 int k;
 cin>>k;
-int arr[100][100];
+int arr[k+1][k+1];
 	if(k%2)
 	draw_ji(k,arr);
 	else
@@ -16,39 +16,24 @@ int arr[100][100];
 	cout<<arr[i][j]<<" ";
 	cout<<'\n';
     }
-    cin.get();
 	return 0;
 }
- draw_ji(int n,int arr[][100])
-{   if(n==1) 	
-{
-arr[1][1]=1;
-return 0;
-}
-else{
-int draw_ou(int n,int arr[][100]);
-	draw_ou(n-1,arr);
+void draw_ji(int n,int arr[][])
+{   void draw_ou(int);
+	draw_ou(n-1);
 	arr[1][1]=(n-1)^2+1;
-	for(int j=2;j<=n;j++)
+	for(j=2;j<=n;j++)
 	arr[j][1]=arr[1][j-1]+1;
-	for(int i=2;i<=n;i++)
+	for(i=2;i<=n;i++)
 	arr[n][i]=arr[n][i-1]+1;
 }
-}
-int draw_ou(int n,int arr[][100])
-{   if(n==1) 	{
-arr[1][1]=1;
-return 0;
-}
-else
-{
-int draw_ji(int n,int arr[][100]);
-	draw_ji(n-1,arr);
+void draw_ou(int n,int arr[][])
+{   void draw_ji(int);
+	draw_ji(n-1);
 	arr[n][n]=(n-1)^2+1;
-	for(int j=n-1;j>=1;j--)
+	for(j=n-1;j>=1;j--)
 	arr[j][n]=arr[j+1][n]+1;
-	for(int i=n-1;i>=1;i--)
+	for(i=n-1;i>=1;i--)
 	arr[1][i]=arr[1][i-1]+1;
-}
  } 
 
